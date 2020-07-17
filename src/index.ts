6a5ce20a1c7f58d32bbcadd80cd0cdc7fe0646ba
Gabriel Morales <gabrielmorales201564@hotmail.com>
2020-07-16 22:14:53 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import  {PORT} from './config'
 import router from './routes/index';
 import express ,{json}from 'express';
+import  cors from 'cors';
 const app =express();
 
+app.use(cors({ origin: true}));
 app.use(json());
 app.use(express.urlencoded({extended:false}));
-
 app.use(router);
 app.listen(PORT, () => {
     console.log(`escuchando en http://localhost:${PORT}`);
