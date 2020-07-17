d323bfe9f5ab9507255311e7054e8a612c56e077
Gabriel Morales <gabrielmorales201564@hotmail.com>
2020-07-16 21:24:40 -0500

    config database

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -6,8 +6,10 @@ import {QueryResult} from 'pg';
 export const getcommunitys = async (req:Request,res:Response):Promise<Response> =>{
 
     try {
+        const client = await pool.connect();
         const resp : QueryResult = await pool.query("select nombre_comunidad as ciudad,sum(cantidad)  as Cantidad from nacimientos ,ciudad where nacimientos.id_ciudad = ciudad.id_ciudad group by nombre_comunidad order by nombre_comunidad");
         return res.status(200).json(resp.rows)
+        client.release();
     } catch (error) {
         return res.status(500).json("Internal Server Error"+error);
     }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,7 @@
 import { Pool } from "pg";
-export const pool = new Pool ({
-
-    user:'postgres',
-    host:'localhost',
-    password:'75315986245',
-    database:'Base_relacional',
-    port:5432
-
-})
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
